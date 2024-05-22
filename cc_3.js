33c17b13cfa3ac4b5f3b2b7aa7d08d2c985b8d22
zatb <zatbolanos@gmail.com>
2024-05-21 23:38:38 -0400

    Test accelerate and brake methods on Car instances.

--- cc_3.js
+++ cc_3.js
@@ -13,18 +13,37 @@ class Car {
 
     //Implement an accelerate method in the Car class that increases the car's speed by 10 km/h and logs the new speed to the console.
 
-accelerate(){
+    accelerate(){
     this.current_kmh += accelerate_kmh;
     console.log ("The new speed of the " + this.make + " after accelerating " + accelerate_kmh + "KM/h is" + this.current_kmh + "KM/h.");
-}
+    }
 
 //Implement a brake method that decreases the car's speed by 5 km/h and logs the new speed to the console.
 
 brake(){
     this.current_kmh -= brake_kmh;
     console.log ("The new speed of the " + this.make + "after breaking" + brake_kmh + "KM/h is" + this.current_kmh + "KM/h.");
+    }
 }
 
+//Create two Car objects using the provided test data.
+//Car 1: A 'BMW' starting at 120 km/h.
+car1 = new Car("BMW", 120);
 
-}
+//Car 2: A 'Mercedes' starting at 95 km/h.
+car2 = new Car("Mercedes", 95);
+
+//Test the accelerate and brake methods on each object, calling each method multiple times to demonstrate functionality.
+console.log("Testing Car 1:");
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+
+console.log("Testing Car 2:");
+car2.accelerate();
+car2.brake();
+car2.brake();
+
+
+
   
