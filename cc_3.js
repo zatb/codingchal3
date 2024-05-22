71d9b06538083a386774790ab4cc2f2b4fdea88a
zatb <zatbolanos@gmail.com>
2024-05-21 23:03:34 -0400

    Create Car class with constructor for make and speed.

---
+++ cc_3.js
@@ -0,0 +1,14 @@
+//U36432650
+
+//Implement a car class with a constructor that initializes the make (brand of the car) and the speed (curren speed in km/h) properites.
+
+accelerate_kmh = 10
+brake_kmh = 5
+
+class Car {
+    constructor(make, accelerate_kmh) {
+      this.make = make;
+      this.current_kmh = Number(this.current_kmh);
+    }
+ }
+  
